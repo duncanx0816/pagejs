8345609634b182454ca24a4dafbe2bdfa1f867bf
duncanx0816 <xiongdengkun@163.com>
2022-11-10 18:32:24 +0800

    Update av2048.js

--- av2048.js
+++ av2048.js
@@ -8,9 +8,10 @@
 // @match        http*://dp227.xyz/pw/*
 // @match        http*://down.dataaps.com/list.php?name=*
 // @match        http*://ww1.k00ppc.com/*
-// @match        http*://www.juejin.cn/*
+// @match        http*://juejin.cn/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=fc1y.xyz
-// @grant        unsafeWindow
+// @grant        unsafeWndiow
+// @grant        window.close
 // @grant        GM_addStyle
 // @grant        GM_deleteValue
 // @grant        GM_listValues
@@ -32,10 +33,9 @@
 // @grant        GM_notification
 // @grant        GM_setClipboard
 // @grant        GM_info
-// @grant        window.close
 // ==/UserScript==
 
-let init=()=>{
+const init=()=>{
     unsafeWindow.GM_setValue = GM_setValue
     unsafeWindow.GM_getValue = GM_getValue
     unsafeWindow.GM_addStyle = GM_addStyle
@@ -54,27 +54,37 @@ let init=()=>{
     unsafeWindow.GM_getTab = GM_getTab
     unsafeWindow.GM_saveTab = GM_saveTab
     unsafeWindow.GM_getTabs = GM_getTabs
-    unsafeWindow.GM_notification = GM_notification
+    usafeWinnotificatindow.GM_on = GM_notification
     unsafeWindow.GM_setClipboard = GM_setClipboard
     unsafeWindow.GM_info = GM_info
 }
 
-const juejin=function(){
-    document.querySelector('.recommend-box')
-    document.querySelector('.recommend-box').remove()
-    document.querySelector('.wrap.category-course-recommend').remove()
-    document.querySelector('.article-end').remove()
-    document.querySelector('.sidebar-bd-entry').remove()
-    document.querySelector('.sidebar-block.app-download-sidebar-block.shadow').remove()
-    document.querySelector('.sidebar-block.author-block.pure').remove()
-    document.querySelector('.sidebar-block.related-entry-sidebar-block.shadow').remove()
-    document.querySelector('.main-nav-list').remove()
+const deleteElmByCSS=(str_css)=>{
+    if(document.querySelector(str_css)){
+        document.querySelector(str_css).remove()
+    }
+}
+
+const juejin=()=>{
+    setInterval(()=>{
+        deleteElmByCSS('.wrap.category-course-recommend')
+        deleteElmByCSS('.sidebar-block.author-block.pure')
+        deleteElmByCSS('.recommend-box')
+        deleteElmByCSS('.article-end')
+        deleteElmByCSS('.sidebar-bd-entry')
+        deleteElmByCSS('.sidebar-block.app-download-sidebar-block.shadow')
+        deleteElmByCSS('.sidebar-block.shadow')
+        deleteElmByCSS('.main-nav-list')
+        deleteElmByCSS('.sidebar-block.sticky-block')
+        deleteElmByCSS('.sidebar-block.banner-block')
+    },500)
 }
 
 
 (function() {
     'use strict';
     console.log('test')
+
     if(location.host=="juejin.cn"){
         juejin()
     }else{
@@ -84,6 +94,6 @@ const juejin=function(){
         document.querySelectorAll(".tr3[align='middle']").forEach((el)=>{el.parentNode.removeChild(el)})
         document.querySelectorAll('.apd>a').forEach((el)=>{el.parentNode.removeChild(el)})
         document.querySelectorAll('#td_tpc font').forEach((el)=>{el.parentNode.removeChild(el)})
-        document.querySelectorAll('.tr1.r_one').forEach((el)=>{el.parentNode.removeChild(el)})    
+        document.querySelectorAll('.tr1.r_one').forEach((el)=>{el.parentNode.removeChild(el)})
     }
 })();
