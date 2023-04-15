9b9e9e0e0ed4b4cfc929981e7ff7e07590b6e7b2
duncanx0816 <xiongdengkun@163.com>
2023-04-15 12:03:12 +0800

    Update edc.js

--- edc.js
+++ edc.js
@@ -5,11 +5,17 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://ascentage.mdsol.com/*
+// @match        https://ascentage-klserv.mdsol.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mdsol.com
-// @grant        none
+// @grant        GM_openInTab
+// @grant        window.close
 // @copyright      file://C:\Users\lckfb-xdk\Desktop\edc.js
 // ==/UserScript==
 
+const get_page = async (url) => {
+    let res = await fetch(url).then((res) => res.text());
+    return new DOMParser().parseFromString(res, "text/html");
+};
 
 const get_sub_page = async (doc, target) => {
     let formdata = new FormData();
@@ -35,122 +41,166 @@ const get_sub_page = async (doc, target) => {
 class Subject {
     constructor(doc) {
         this.fstDrug;
-        this.eotInfo={};
-        this.vstInfo = []
+        this.eotInfo = {};
+        this.vstInfo = [];
+        this.pdInfo = {};
 
-        this.doc = doc
-        this.subID = this.doc.querySelector('#_ctl0_PgHeader_TabTextHyperlink3').textContent.trim()
-        this.run()
+        this.doc = doc;
+        this.subID = this.doc
+            .querySelector("#_ctl0_PgHeader_TabTextHyperlink3")
+            .textContent.trim();
+        this.run();
     }
 
     run = async () => {
-        let trs = this.doc.querySelectorAll("#GRID tr")
-        this.parse_vst_date(this.doc)
+        let trs = this.doc.querySelectorAll("#GRID tr");
+        this.parse_vst_date(this.doc);
 
-        let aa = trs[trs.length - 1].querySelectorAll('a')
-        aa = [...aa]
-        let res = aa.map(async a => get_sub_page(this.doc, a.href.split("'")[1]).then(doc => this.parse_vst_date(doc, true)))
-        await Promise.all(res)
+        let aa = trs[trs.length - 1].querySelectorAll("a");
+        aa = [...aa];
+        let res = aa.map(async (a) =>
+            get_sub_page(this.doc, a.href.split("'")[1]).then((doc) =>
+                this.parse_vst_date(doc, true)
+            ));
+        await Promise.all(res);
 
-        await this.parse_1st_drug()
-        await this.parse_eot()
+        let tasks=this.vstInfo.map(async vst => this.parse_vst_PD(vst[2]))
+        await Promise.all(tasks)
+
+        // await this.parse_1st_drug();
+        // await this.parse_eot();
 
         let blob = new Blob(
-            [JSON.stringify({
-                subID: this.subID,
-                href: location.href,
-                fstDrug: this.fstDrug,
-                vstInfo: this.vstInfo,
-                eotInfo: this.eotInfo,
-            })], {
-                type: "application/json"
+            [
+                JSON.stringify({
+                    subID: this.subID,
+                    href: location.href,
+                    fstDrug: this.fstDrug,
+                    vstInfo: this.vstInfo,
+                    eotInfo: this.eotInfo,
+                    pdInfo: this.pdInfo,
+                }),
+            ], {
+                type: "application/json",
             }
         );
-        let a = this.doc.createElement('a')
-        a.download = `${this.subID}.${(new Date()).getTime()}.json`
-        a.href = URL.createObjectURL(blob)
-        a.click()
-        window.close()
-    }
+        let a = this.doc.createElement("a");
+        a.download = `${this.subID}.${new Date().getTime()}.json`;
+        a.href = URL.createObjectURL(blob);
+        a.click();
+        window.close();
+    };
 
     parse_eot = async () => {
-        if(this.doc.querySelector('a.leftNaveTableRowLink[title^="治疗终止"]')){
-        let link = this.doc.querySelector(
-            'a.leftNaveTableRowLink[title^="治疗终止"]'
-        ).href;
-        let doc = await fetch(link)
-            .then((res) => res.text())
-            .then((res) => new DOMParser().parseFromString(res, "text/html"));
+        if (this.doc.querySelector('a.leftNaveTableRowLink[title^="治疗终止"]')) {
+            let link = this.doc.querySelector(
+                'a.leftNaveTableRowLink[title^="治疗终止"]'
+            ).href;
+            let doc = await fetch(link)
+                .then((res) => res.text())
+                .then((res) => new DOMParser().parseFromString(res, "text/html"));
 
-        let k1 = doc
-            .querySelectorAll("#_ctl0_Content_R>table")[1]
-            .querySelector("tbody>tr>td")
-            .textContent.trim();
-        let v1 = doc
-            .querySelectorAll("#_ctl0_Content_R>table")[1]
-            .querySelector("tbody>tr>td:nth-child(3)")
-            .textContent.trim();
-        let k2 = doc
-            .querySelectorAll("#_ctl0_Content_R>table")[2]
-            .querySelector("tbody>tr>td")
-            .textContent.trim();
-        let v2 = doc
-            .querySelectorAll("#_ctl0_Content_R>table")[2]
-            .querySelector("tbody>tr>td:nth-child(3)")
-            .textContent.trim();
+            let k1 = doc
+                .querySelectorAll("#_ctl0_Content_R>table")[1]
+                .querySelector("tbody>tr>td")
+                .textContent.trim();
+            let v1 = doc
+                .querySelectorAll("#_ctl0_Content_R>table")[1]
+                .querySelector("tbody>tr>td:nth-child(3)")
+                .textContent.trim();
+            let k2 = doc
+                .querySelectorAll("#_ctl0_Content_R>table")[2]
+                .querySelector("tbody>tr>td")
+                .textContent.trim();
+            let v2 = doc
+                .querySelectorAll("#_ctl0_Content_R>table")[2]
+                .querySelector("tbody>tr>td:nth-child(3)")
+                .textContent.trim();
 
-        let res = {}
-        res[k1] = v1
-        res[k2] = v2
-        this.eotInfo= res
-    }
-    }
+            let res = {};
+            res[k1] = v1;
+            res[k2] = v2;
+            this.eotInfo = res;
+        }
+    };
 
     parse_vst_date = (doc, sub = false) => {
-        let trs = sub ? doc.querySelectorAll("#_ctl0_Content__ctl0_GRID tr") : doc.querySelectorAll("#GRID tr")
-        let trs_ = [...trs]
-        trs_ = trs_.slice(0, trs_.length - 1)
-        let res = trs_.map(
-            tr => {
-                let tds = tr.querySelectorAll("td")
-                let td_vst = tds[1]
-                let td_dt = tds[2]
-                try {
-                    return [
-                        td_vst.textContent.trim(),
-                        td_dt.textContent.trim(),
-                        td_vst.querySelector("a") && td_vst.querySelector("a").href,
-                    ]
-                } catch {
-                    return ['e', 'e', 'e']
+        let trs = sub ?
+            doc.querySelectorAll("#_ctl0_Content__ctl0_GRID tr") :
+            doc.querySelectorAll("#GRID tr");
+        let trs_ = [...trs];
+        let res = trs_.slice(0, trs_.length - 1).map((tr) => {
+            let [, td_vst, td_dt] = tr.querySelectorAll("td");
+            try {
+                let visit = td_vst.textContent.trim();
+                let date = td_dt.textContent.trim();
+                let href =
+                    td_vst.querySelector("a") && td_vst.querySelector("a").href;
+                if (date && href && href.startsWith('http')) {
+                    return [visit, date, href];
                 }
+            } catch {
+                return;
             }
-        )
-        this.vstInfo = [...this.vstInfo, ...res]
-    }
+        }).filter(i => i);
+        this.vstInfo = [...this.vstInfo, ...res];
+    };
 
-    parse_1st_drug=async ()=>{
+    parse_vst_PD = async (url) => {
+        let doc = await get_page(url);
+        let links = [
+                ...doc.querySelectorAll(
+                    "#_ctl0_LeftNav_EDCTaskList_TblTaskItems a.leftNaveTableRowLink"
+                ),
+            ]
+            .filter((a) => a.textContent.includes("Pharmacodynamic"))
+            .map((a) => a.href);
+
+        if (links.length) {
+            let doc = await get_page(links[0]);
+            let pd_samples = [...doc.querySelectorAll("#log>tbody>tr")].map((tr) => [...tr.childNodes].map((td) => td.textContent.trim()));
+            let header = [...doc.querySelectorAll(".crf_preText")].map((a) =>
+                a.textContent.trim()
+            );
+            let content = [...doc.querySelectorAll(".crf_dataPoint")].map((a) =>
+                a.textContent.trim()
+            );
+            this.pdInfo[url] = {
+                header,
+                content,
+                pd_samples
+            };
+        }
+    };
+
+    parse_1st_drug = async () => {
         let url_prefix = location.href.split("/SubjectPage.aspx")[0];
-        let el = this.doc.querySelector( "#_ctl0_LeftNav_EDCTaskList_TblTaskItems tr:nth-child(3)>td:nth-child(2)>a" );
+        let el = this.doc.querySelector(
+            "#_ctl0_LeftNav_EDCTaskList_TblTaskItems tr:nth-child(3)>td:nth-child(2)>a"
+        );
         let link = new URL(el.href);
-        link= `${url_prefix}/InstancePage.aspx${link.search}`;
-        let res=await fetch(link).then(res=>res.text())
-        let doc = new DOMParser().parseFromString(res, "text/html")
-        let trs=doc.querySelectorAll("#_ctl0_LeftNav_EDCTaskList_TblTaskItems>tbody>tr")
-        let a=trs[trs.length-1].querySelector("td:nth-child(2)>a")
+        link = `${url_prefix}/InstancePage.aspx${link.search}`;
+        let res = await fetch(link).then((res) => res.text());
+        let doc = new DOMParser().parseFromString(res, "text/html");
+        let trs = doc.querySelectorAll(
+            "#_ctl0_LeftNav_EDCTaskList_TblTaskItems>tbody>tr"
+        );
+        let a = trs[trs.length - 1].querySelector("td:nth-child(2)>a");
 
         let link_;
-        if(a.text.trim()=='第1周期服药记录'){
-            link_=new URL(a.href)
-            link_= `${url_prefix}/InstancePage.aspx${link_.search}`
+        if (a.text.trim() == "第1周期服药记录") {
+            link_ = new URL(a.href);
+            link_ = `${url_prefix}/InstancePage.aspx${link_.search}`;
         }
 
-        if(link_){
+        if (link_) {
             let res = await fetch(link_).then((res) => res.text());
             let doc = new DOMParser().parseFromString(res, "text/html");
-            this.fstDrug= doc.querySelector("#log tr:nth-child(2)>td:nth-child(2)>a").text.trim();
+            this.fstDrug = doc
+                .querySelector("#log tr:nth-child(2)>td:nth-child(2)>a")
+                .text.trim();
         }
-    }
+    };
 }
 
 
@@ -162,19 +212,19 @@ class SubjectList {
     }
 
     run = async () => {
+        this.parse_table(this.doc);
+
         let aa = this.doc.querySelectorAll("#_ctl0_Content_ListDisplayNavigation_DlPagination td>a")
-        aa = [...aa]
-        let res = aa.map(async a => get_sub_page(this.doc, a.href.split("'")[1]).then(doc => this.parse_table(doc)))
+        let res = [...aa].slice(1,aa.length).map(async a => get_sub_page(this.doc, a.href.split("'")[1]).then(doc => this.parse_table(doc)))
         await Promise.all(res)
 
-        this.info.forEach((item) => window.open(item[1]));
+        this.info.forEach((item) => GM_openInTab(item[1]));
         window.close()
     }
 
     parse_table = (doc) => {
         let aa = doc.querySelectorAll("#_ctl0_Content_ListDisplayNavigation_dgObjects tr>td:nth-child(2)>a")
-        aa = [...aa]
-        let res = aa.map(a => [a.textContent.trim(), a.href])
+        let res = [...aa].map(a => [a.textContent.trim(), a.href])
         this.info = [...this.info, ...res]
     }
 }
@@ -183,9 +233,9 @@ class SubjectList {
 (function () {
     'use strict';
 
-    if (location.pathname.endsWith('HomePage.aspx') & location.search.startsWith('?LD_StudySiteID=')) {
-        console.log('run')
-        new SubjectList(document);
+    if (location.pathname.endsWith('HomePage.aspx') && location.search.startsWith('?LD_StudySiteID=')) {
+        let ss= new SubjectList(document);
+        console.log(ss.info)
     } else if (location.pathname.endsWith('SubjectPage.aspx')) {
         new Subject(document);
     }
