487a95992a83ebb368e6965e69fe827da493a1f2
duncanx0816 <xiongdengkun@163.com>
2023-05-23 20:54:11 +0800

    Update av2048.js

--- av2048.js
+++ av2048.js
@@ -39,205 +39,219 @@
 // @grant        GM_info
 // ==/UserScript==
 
-const jq = $
-unsafeWindow.jq = unsafeWindow.jq ? unsafeWindow.jq : jq
+const jq = $;
+unsafeWindow.jq = unsafeWindow.jq ? unsafeWindow.jq : jq;
 
 const init = () => {
-    unsafeWindow.GM_setValue = GM_setValue
-    unsafeWindow.GM_getValue = GM_getValue
-    unsafeWindow.GM_addStyle = GM_addStyle
-    unsafeWindow.GM_deleteValue = GM_deleteValue
-    unsafeWindow.GM_listValues = GM_listValues
-    unsafeWindow.GM_addValueChangeListener = GM_addValueChangeListener
-    unsafeWindow.GM_removeValueChangeListener = GM_removeValueChangeListener
-    unsafeWindow.GM_log = GM_log
-    unsafeWindow.GM_getResourceText = GM_getResourceText
-    unsafeWindow.GM_getResourceURL = GM_getResourceURL
-    unsafeWindow.GM_registerMenuCommand = GM_registerMenuCommand
-    unsafeWindow.GM_unregisterMenuCommand = GM_unregisterMenuCommand
-    unsafeWindow.GM_openInTab = GM_openInTab
-    unsafeWindow.GM_xmlhttpRequest = GM_xmlhttpRequest
-    unsafeWindow.GM_download = GM_download
-    unsafeWindow.GM_getTab = GM_getTab
-    unsafeWindow.GM_saveTab = GM_saveTab
-    unsafeWindow.GM_getTabs = GM_getTabs
-    unsafeWindow.GM_on = GM_notification
-    unsafeWindow.GM_setClipboard = GM_setClipboard
-    unsafeWindow.GM_info = GM_info
-}
+  unsafeWindow.GM_setValue = GM_setValue;
+  unsafeWindow.GM_getValue = GM_getValue;
+  unsafeWindow.GM_addStyle = GM_addStyle;
+  unsafeWindow.GM_deleteValue = GM_deleteValue;
+  unsafeWindow.GM_listValues = GM_listValues;
+  unsafeWindow.GM_addValueChangeListener = GM_addValueChangeListener;
+  unsafeWindow.GM_removeValueChangeListener = GM_removeValueChangeListener;
+  unsafeWindow.GM_log = GM_log;
+  unsafeWindow.GM_getResourceText = GM_getResourceText;
+  unsafeWindow.GM_getResourceURL = GM_getResourceURL;
+  unsafeWindow.GM_registerMenuCommand = GM_registerMenuCommand;
+  unsafeWindow.GM_unregisterMenuCommand = GM_unregisterMenuCommand;
+  unsafeWindow.GM_openInTab = GM_openInTab;
+  unsafeWindow.GM_xmlhttpRequest = GM_xmlhttpRequest;
+  unsafeWindow.GM_download = GM_download;
+  unsafeWindow.GM_getTab = GM_getTab;
+  unsafeWindow.GM_saveTab = GM_saveTab;
+  unsafeWindow.GM_getTabs = GM_getTabs;
+  unsafeWindow.GM_on = GM_notification;
+  unsafeWindow.GM_setClipboard = GM_setClipboard;
+  unsafeWindow.GM_info = GM_info;
+};
 
 const juejin = () => {
-    let flag = true
-    let ntime = 100
-    let timer = setInterval(() => {
-        --ntime
-        if (ntime < 0) {
-            clearInterval(timer)
-        }
-        jq('.wrap.category-course-recommend').remove()
-        jq('.sidebar-block.author-block.pure').remove()
-        jq('.recommend-box').remove()
-        jq('.article-end').remove()
-        jq('.sidebar-bd-entry').remove()
-        jq('.sidebar-block.app-download-sidebar-block.shadow').remove()
-        jq('.sidebar-block.shadow').remove()
-        jq('.main-nav-list').remove()
-        jq('.sidebar-block.sticky-block').remove()
-        jq('.sidebar-block.banner-block').remove()
-        jq('div.guide-collect-popover').hide()
-        jq('div.author-info-block').next('img').hide()
-        jq('#comment-box > div.comment-list-wrapper').hide()
-        jq('#comment-box > div.container.hot-list').hide()
+  let flag = true;
+  let ntime = 100;
+  let timer = setInterval(() => {
+    --ntime;
+    if (ntime < 0) {
+      clearInterval(timer);
+    }
+    jq(".wrap.category-course-recommend").remove();
+    jq(".sidebar-block.author-block.pure").remove();
+    jq(".recommend-box").remove();
+    jq(".article-end").remove();
+    jq(".sidebar-bd-entry").remove();
+    jq(".sidebar-block.app-download-sidebar-block.shadow").remove();
+    jq(".sidebar-block.shadow").remove();
+    jq(".main-nav-list").remove();
+    jq(".sidebar-block.sticky-block").remove();
+    jq(".sidebar-block.banner-block").remove();
+    jq("div.guide-collect-popover").hide();
+    jq("div.author-info-block").next("img").hide();
+    jq("#comment-box > div.comment-list-wrapper").hide();
+    jq("#comment-box > div.container.hot-list").hide();
 
-        let btn_old = jq('button.btn.meiqia-btn')
-        if (flag && btn_old.length) {
-            let btn_new = btn_old.clone()
-            btn_old.replaceWith(btn_new) // remove all listeners on btn_old
-            btn_new.click((e) => {
-                e.preventDefault();
-                e.stopImmediatePropagation();
-                jq('#comment-box > div.comment-list-wrapper').toggle()
-                jq('#comment-box > div.container.hot-list').toggle()
-            })
-            flag = false
-            console.log('done')
-        }
-    }, 100)
-}
+    let btn_old = jq("button.btn.meiqia-btn");
+    if (flag && btn_old.length) {
+      let btn_new = btn_old.clone();
+      btn_old.replaceWith(btn_new); // remove all listeners on btn_old
+      btn_new.click((e) => {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        jq("#comment-box > div.comment-list-wrapper").toggle();
+        jq("#comment-box > div.container.hot-list").toggle();
+      });
+      flag = false;
+      console.log("done");
+    }
+  }, 100);
+};
 
 const torlook = () => {
-    let aa = jq("span.magnet > a")
-    aa = [...aa]
-    aa.forEach(
-        a => {
-            let url = `${location.origin}/${a.dataset.src}?fancybox=true`
-            let options = {
-                referrer: location.href,
-                credentials: "include",
-                headers: {}
-            }
-            fetch(url, options).then(response => response.text()).then(data => {
-                jq(a).replaceWith(jq("<a/>", {
-                    class: "dl magneto ut-download-url",
-                    href: `magnet:${data.split("magnet:")[1].split("'")[0]}`
-                }))
-                console.log(`magnet:${data.split("magnet:")[1].split("'")[0]}`)
-            })
-        }
-    )
-}
+  let aa = jq("span.magnet > a");
+  aa = [...aa];
+  aa.forEach((a) => {
+    let url = `${location.origin}/${a.dataset.src}?fancybox=true`;
+    let options = {
+      referrer: location.href,
+      credentials: "include",
+      headers: {},
+    };
+    fetch(url, options)
+      .then((response) => response.text())
+      .then((data) => {
+        jq(a).replaceWith(
+          jq("<a/>", {
+            class: "dl magneto ut-download-url",
+            href: `magnet:${data.split("magnet:")[1].split("'")[0]}`,
+          })
+        );
+        console.log(`magnet:${data.split("magnet:")[1].split("'")[0]}`);
+      });
+  });
+};
 
-const rargb_to=()=>{
-    const parse_link=async (url)=>{
-        let res=await fetch(url).then(res=>res.text())
-        let doc = new DOMParser().parseFromString(res, "text/html")
-        for(let a of doc.querySelectorAll('.lista>a')){
-            if(a.href && a.href.startsWith('magnet:')) return a.href
-        }
+const rargb_to = () => {
+  const parse_link = async (url) => {
+    let res = await fetch(url).then((res) => res.text());
+    let doc = new DOMParser().parseFromString(res, "text/html");
+    for (let a of doc.querySelectorAll(".lista>a")) {
+      if (a.href && a.href.startsWith("magnet:")) return a.href;
     }
+  };
 
-    for(let a of document.querySelectorAll('.lista2t tr.lista2 td.lista:nth-child(2)>a')){
-        parse_link(a.href).then(magnet=>{
-            if(magnet){
-                a.parentElement.innerHTML=`<a target="_blank" href="${magnet}" onclick="navigator.clipboard.writeText(event.target.href); return false">[Torrent]</a>  `+a.outerHTML
-            }
-        })
+  for (let a of document.querySelectorAll(
+    ".lista2t tr.lista2 td.lista:nth-child(2)>a"
+  )) {
+    parse_link(a.href).then((magnet) => {
+      if (magnet) {
+        a.parentElement.innerHTML =
+          `<a target="_blank" href="${magnet}" onclick="navigator.clipboard.writeText(event.target.href); return false">[Torrent]</a>  ` +
+          a.outerHTML;
+      }
+    });
+  }
+};
+
+const get_page = async (url) => {
+  let res = await fetch(url).then((res) => res.text());
+  return new DOMParser().parseFromString(res, "text/html");
+};
+
+class AV2048 {
+  constructor() {
+    this.info = [];
+    this.blockAD();
+    this.run();
+  }
+
+  run = async () => {
+    let url_index = "https://data.3lv2g.com/2048/";
+    if (location.href == url_index) {
+      let req1 = [...Array(10).keys()].map(async (idx) =>
+        this.parse(`${url_index}thread.php?fid-13-page-${idx + 1}.html`)
+      );
+      let req2 = [...Array(10).keys()].map(async (idx) =>
+        this.parse(`${url_index}thread.php?fid-14-page-${idx + 1}.html`)
+      );
+      let req3 = [...Array(10).keys()].map(async (idx) =>
+        this.parse(`${url_index}thread.php?fid-3-page-${idx + 1}.html`)
+      );
+      await Promise.all([...req1, ...req2, ...req3]).then(() => this.update());
     }
-}
+  };
 
-class AV2048{
-    constructor(){
-        this.blockAD();
-        this.parse();
+  parse = async (url) => {
+    let doc = await get_page(url);
+    let avs = [...doc.querySelectorAll("#ajaxtable td.tal a.subject")]
+      .map((a) => {
+        let link = a.href;
+        let flag = new URL(a.href).pathname.startsWith("/2048/state");
+        let title = a.textContent.trim();
+        let pub_date = a.parentElement.nextElementSibling
+          .querySelector("div.f10.gray")
+          .textContent.trim();
+        return { title, pub_date, link, flag };
+      })
+      .filter((i) => i.flag);
+    this.info = [...this.info, ...avs];
+    console.log([this.info.length, url]);
+  };
+
+  update = () => {
+    console.log(this.info.length);
+    let url = "https://www.wdym9816.top:444/api/av2048/";
+    return fetch(url, {
+      method: "POST",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(this.info),
+    }).then((res) => console.log(res.ok));
+  };
+
+  blockAD = () => {
+    unsafeWindow.setpos = () => {};
+    jq("div.tac").remove();
+    jq(".tr3[align='middle']").remove();
+    jq(".apd>a").remove();
+    jq("#td_tpc font").remove();
+    jq(".tr1.r_one").remove();
+    jq("#td_3733").parent().remove();
+
+    let ntime = 100;
+    let timer = setInterval(() => {
+      --ntime;
+      if (ntime < 0) {
+        clearInterval(timer);
+      }
+      jq("#td_3733").parent().remove();
+    }, 100);
+
+    let a = jq('#read_tpc > a[href*="?name="]')[0];
+
+    if (a && a.href.match(/\?name=(\w{32})/)) {
+      let hash = a.href.match(/\?name=(\w{32})/)[1];
+      let link = `${new URL(a.href).origin}/down.php/${hash}.torrent`;
+      let a_ = jq("<a/>", { href: link, text: link })[0];
+      jq(a).replaceWith(a_);
+
+      let h = jq("#subject_tpc")[0];
+      let h_ = jq("<a/>", { href: link })[0];
+      h_.innerHTML = h.outerHTML;
+      jq(h).replaceWith(h_);
     }
-
-    parse=()=>{
-        this.info = [...document.querySelectorAll("#ajaxtable td.tal a.subject")]
-            .map((a) => {
-              let link = a.href;
-              let flag = new URL(a.href).pathname.startsWith("/2048/state");
-              let title = a.textContent.trim();
-              let pub_date = a.parentElement.nextElementSibling
-                .querySelector("div.f10.gray")
-                .textContent.trim();
-              return { title, pub_date, link, flag };
-            })
-            .filter((i) => i.flag)
-
-        console.log(this.info.length);
-
-        let url="https://www.wdym9816.top:444/api/av2048/"
-        fetch(url, {
-            method: "POST",
-            mode: 'cors',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(this.info)
-        }).then(res=>console.log(res.ok))
-
-    }
-
-    blockAD=()=>{
-        unsafeWindow.setpos = () => {};
-        jq("div.tac").remove();
-        jq(".tr3[align='middle']").remove();
-        jq(".apd>a").remove();
-        jq("#td_tpc font").remove();
-        jq(".tr1.r_one").remove();
-        jq("#td_3733").parent().remove();
-
-        let ntime = 100;
-        let timer = setInterval(() => {
-          --ntime;
-          if (ntime < 0) {
-            clearInterval(timer);
-          }
-          jq("#td_3733").parent().remove();
-        }, 100);
-
-        let a = jq('#read_tpc > a[href*="?name="]')[0];
-
-        if (a && a.href.match(/\?name=(\w{32})/)) {
-            let hash = a.href.match(/\?name=(\w{32})/)[1];
-            let link = `${new URL(a.href).origin}/down.php/${hash}.torrent`;
-            let a_ = jq("<a/>", { href: link, text: link })[0];
-            jq(a).replaceWith(a_);
-
-            let h = jq("#subject_tpc")[0];
-            let h_ = jq("<a/>", { href: link })[0];
-            h_.innerHTML = h.outerHTML;
-            jq(h).replaceWith(h_);
-        }
-    }
+  };
 }
 
 (function () {
-    'use strict';
-    console.log('av2048')
+  "use strict";
+  console.log("av2048");
 
-    if (location.host == "juejin.cn") {
-        juejin()
-    } else if (location.host == 'gw3.torlook.info') {
-        torlook()
-    } else if (location.host == 'rargb.to') {
-        rargb_to()
-    } else {
-        new AV2048()
-        let observer = new MutationObserver(() => {
-            console.log(`changed: ${(new Date()).getTime()}`)
-            observer.disconnect();
-            new AV2048()
-            setTimeout(()=>{
-                observer.observe(
-                    document.querySelector('#ajaxtable>tbody:nth-child(2)'), {
-                        childList: true
-                })
-            }, 2000)
-
-        });
-        observer.observe(
-            document.querySelector('#ajaxtable>tbody:nth-child(2)'), {
-                childList: true
-        })
-
-    }
+  if (location.host == "juejin.cn") {
+    juejin();
+  } else if (location.host == "gw3.torlook.info") {
+    torlook();
+  } else if (location.host == "rargb.to") {
+    rargb_to();
+  } else {
+    new AV2048();
+  }
 })();
