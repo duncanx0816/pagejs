523e9d72dd1703ecb7d0562d5e0d12c5d6897f38
duncanx0816 <xiongdengkun@163.com>
2024-06-01 23:58:58 +0800

    Update AVview.js

--- AVview.js
+++ AVview.js
@@ -1,13 +1,17 @@
 // ==UserScript==
 // @name         AVview
 // @namespace    http://tampermonkey.net/
-// @version      2024-05-25
+// @version      2024-06-01
 // @description  try to take over the world!
 // @author       You
 // @match        http*://q0519.cc/pw/*
 // @match        http*://thza.cc/*
+// @match        http://777630.xyz/*
 // @match        http*://z2311k.xyz/pw/*
+// @match        http*://g8b1.com/*
+// @match        http://www.99ybbc.com/*
 // @match        http*://*/link.aspx?hash=*
+// @match        https://*/list.php?name=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=q0519.cc
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @grant        unsafeWindow
@@ -41,14 +45,42 @@ const get_page = async (url) => {
   return new DOMParser().parseFromString(res, "text/html");
 };
 
+const parse = async (css) => {
+    let avs = [...document.querySelectorAll(css)]
+        .map((a) => {
+            let link = a.href;
+            let title = a.textContent.trim();
+            let pub_date = null;
+
+            let el=a.offsetParent.parentElement
+            if(el.tagName=="TR"){
+                pub_date=el.textContent.match(/\d{4}-\d{2}-\d{2}/);
+                pub_date=pub_date && pub_date[0];
+            }
+
+            return { title, pub_date, link };
+        });
+
+    let url_ = "https://www.wdym9816.top:444/api/update/av2048/";
+    return fetch(url_, {
+        method: "POST",
+        mode: "cors",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(avs),
+    });
+};
+
 (function() {
     'use strict';
     [
         ["#threadlisttableid th.common a.s.xst", "td.t_f img"],
         ["#main tr.tr3.t_one td h3>a", "div.tpc_content img"],
+        ["#ajaxtable tr.tr3.t_one a.subject", "div.tpc_content img"],
+        [".titletablerow a", ".titletablerow img"],
     ].map(async item=>{
         let [css1, css2]=item
         console.log([css1, css2])
+        parse(css1);
         Promise.all(
             [...document.querySelectorAll(css1)].map(
                 async (a) => {
