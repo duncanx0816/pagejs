0d88ef6f3bfeeadf19c0de2d7c3e0be3a52e88ac
duncanx0816 <xiongdengkun@163.com>
2024-11-24 17:34:43 +0800

    Update vixen.js

--- vixen.js
+++ vixen.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         vixen
 // @namespace    http://tampermonkey.net/
-// @version      2024-11-24
+// @version      2024-11-25
 // @description  try to take over the world!
 // @author       You
 // @updateURL    https://raw.githubusercontent.com/duncanx0816/pagejs/main/vixen.js
@@ -59,10 +59,6 @@ class Vixen {
         "div.VideosSidebar__Main-sc-11jbuek-1.AJrQs"
       ).style.display = "none";
     }
-    // let divs = document.querySelectorAll(
-    //   ".Grid__GridContainer-f0cb34-0.bUAzPt.VideoList__VideoListContainer-sc-1u75cgc-0.eliAOo.videos__StyledVideoList-sc-1u2b7uh-3.dTsEcV>div.Grid__Item-f0cb34-1.dSIsBc"
-    // );
-    // await Promise.all(Array.from(divs).map(this.parseVideo));
     this.changeLink();
     await Promise.all(this.info.props.pageProps.edges.map(this.parseVideo));
     let nVideo = Object.keys(this.videos).length;
@@ -100,7 +96,7 @@ class Vixen {
         a.href=link;
         a.innerHTML = `<i class="fa-solid fa-magnet" style="font-size: 18px;color: blue;"></i> ${a.innerText}`;
       } else if (link.endsWith('.mkv')){
-        a.innerHTML = `<i class="fa-solid fa-video" style="font-size: 18px;color: grey;"></i> ${a.innerText}`;
+        a.innerHTML = `<i class="fa-solid fa-video" style="font-size: 18px;color: green;"></i> ${a.innerText}`;
       }else{
         a.innerHTML = `<i class="fa-solid fa-video" style="font-size: 18px;color: blue;"></i> ${a.innerText}`;
       }
