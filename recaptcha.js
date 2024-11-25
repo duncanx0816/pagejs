89f48c9646a6af32630abdd06d1ea110a733a97a
duncanx0816 <xiongdengkun@163.com>
2024-11-25 20:15:38 +0800

    Update recaptcha.js

--- recaptcha.js
+++ recaptcha.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         recaptcha
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      2024-11-25
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.google.com/recaptcha/api2/anchor?*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=btdig.com
 // @grant        none
 // @run-at       document-start
+// @updateURL    https://github.com/duncanx0816/pagejs/raw/refs/heads/main/recaptcha.js
+// @downloadURL  https://github.com/duncanx0816/pagejs/raw/refs/heads/main/recaptcha.js
 // ==/UserScript==
 
 
