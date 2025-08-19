9f61f0b5aaa97ecda13e3a27a83bf05f0c9831fb
duncanx0816 <xiongdengkun@163.com>
2025-08-19 11:41:34 +0800

    Update xsijishe.js

--- xsijishe.js
+++ xsijishe.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         xsijishe
 // @namespace    http://tampermonkey.net/
-// @version      20250819
+// @version      20250820
 // @description  try to take over the world!
 // @author       You
 // @match        https://xsijishe.com/home.php?*do=favorite*
@@ -15,34 +15,52 @@ const get_page = async (url) => {
 };
 
 
-(function() {    
-    
-    [...document.querySelectorAll('#favorite_ul>li')].forEach(async e=>{
-        let href=e.querySelector('a:last-of-type').href
-        let doc=await get_page(href)
-        let srt = "location2={href:''};location2.assign=function(val){this.href=val};location2.replace=function(val){this.href=val};"+doc.scripts[0].innerHTML.replaceAll('location','location2')+"location2;"
-    
-        try{
-            let res=window.eval(srt)
-            let newLink=res?.href || res
+const delFav = async (href) => {
+    event.preventDefault();
+    let res = await fetch(href).then((res) => res.text());
+    let doc = new DOMParser().parseFromString(res, "text/html");
+    console.log(doc.querySelector('input[name="formhash"]').value);
+
+    const formData = new FormData();
+    formData.append('referer', "https://xsijishe.com/home.php?mod=space&do=favorite&view=me");
+    formData.append('formhash', doc.querySelector('input[name="formhash"]').value);
+    formData.append('deletesubmit', true);
+    formData.append('deletesubmitbtn', true);
+
+    fetch(href, { method: 'POST', body: formData })
+    return false
+}
+
+
+(function() {
+    unsafeWindow.delFav=delFav;
+
+    [...document.querySelectorAll('#favorite_ul>li')].forEach(async e => {
+        e.querySelector('a').attributes.onclick.value = "delFav(this.href); this.parentNode?.remove();"
+
+        let href = e.querySelector('a:last-of-type').href
+        let doc = await get_page(href)
+        let srt = "location2={href:''};location2.assign=function(val){this.href=val};location2.replace=function(val){this.href=val};" + doc.scripts[0].innerHTML.replaceAll('location', 'location2') + "location2;"
+
+        try {
+            let res = window.eval(srt)
+            let newLink = res?.href || res
             console.log([res, newLink])
-    
+
             // document.getElementById('test')?.remove();
             // script = document.createElement('script');
             // script.id = 'test';
             // script.textContent = srt;
             // document.body.appendChild(script);
             // console.log(location2);
-    
+
             let doc2 = await get_page(newLink)
-            let imgText = [...doc2.querySelectorAll('img.zoom')].map(e=>{e.src=e.attributes.file.value; return e.outerHTML}).join('')
-            e.insertAdjacentHTML('beforeend',`<div style="overflow:auto"><div style="display: flex; flex-wrap:no-wrap">${imgText}</div></div>`)
-            e.querySelectorAll('img.zoom').forEach(e=>{e.style.height="200px"; e.style.width="fit-content"})
-    
-        }catch(e){
+            let imgText = [...doc2.querySelectorAll('img.zoom')].map(e => { e.src = e.attributes.file.value; return e.outerHTML }).join('')
+            e.insertAdjacentHTML('beforeend', `<div style="overflow:auto"><div style="display: flex; flex-wrap:no-wrap">${imgText}</div></div>`)
+            e.querySelectorAll('img.zoom').forEach(e => { e.style.height = "200px"; e.style.width = "fit-content" })
+        } catch (e) {
             console.log(href, e, srt)
         }
-    
     })
 
 })();
