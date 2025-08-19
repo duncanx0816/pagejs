e5e61dc96cfb69e37275ab2025fe5da4e672e8c2
duncanx0816 <xiongdengkun@163.com>
2025-08-19 10:01:34 +0800

    Update xsijishe.js

--- xsijishe.js
+++ xsijishe.js
@@ -15,11 +15,11 @@ const get_page = async (url) => {
 };
 
 
-class MyLocation {
-    constructor() { this.href = '' }
-    assign(url) { this.href = url }
-    replace(url) { this.href = url }
-};
+// class MyLocation {
+//     constructor() { this.href = '' }
+//     assign(url) { this.href = url }
+//     replace(url) { this.href = url }
+// };
 
 
 (function() {
@@ -29,17 +29,24 @@ class MyLocation {
     let href=e.querySelector('a:last-of-type').href
     let doc=await get_page(href)
     let srt = "location2={href:''};location2.assign=function(val){this.href=val};location2.replace=function(val){this.href=val};"+doc.scripts[0].innerHTML.replaceAll('location','location2')+"location2;"
-    let location2 = new MyLocation();
+    // let location2 = new MyLocation();
 
     try{
-        let res=window.eval(srt)
-        let newLink=res?.href || res
-        console.log([res, newLink])
+        // let res=window.eval(srt)
+        // let newLink=res?.href || res
+        // console.log([res, newLink])
 
-        let doc2 = await get_page(newLink)
-        let imgText = [...doc2.querySelectorAll('img.zoom')].map(e=>{e.src=e.attributes.file.value; return e.outerHTML}).join('')
-        e.insertAdjacentHTML('beforeend',`<div style="overflow:auto"><div style="display: flex; flex-wrap:no-wrap">${imgText}</div></div>`)
-        e.querySelectorAll('img.zoom').forEach(e=>{e.style.height="200px"; e.style.width="fit-content"})
+        document.getElementById('test')?.remove();
+        script = document.createElement('script');
+        script.id = 'test';
+        script.textContent = srt;
+        document.body.appendChild(script);
+        console.log(location2);
+
+        // let doc2 = await get_page(newLink)
+        // let imgText = [...doc2.querySelectorAll('img.zoom')].map(e=>{e.src=e.attributes.file.value; return e.outerHTML}).join('')
+        // e.insertAdjacentHTML('beforeend',`<div style="overflow:auto"><div style="display: flex; flex-wrap:no-wrap">${imgText}</div></div>`)
+        // e.querySelectorAll('img.zoom').forEach(e=>{e.style.height="200px"; e.style.width="fit-content"})
 
     }catch(e){
         console.log(href, e, srt)
