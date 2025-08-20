52574846867623f3961240c3a2d052948f0c74a3
duncanx0816 <xiongdengkun@163.com>
2025-08-20 14:50:05 +0800

    Update xsijishe.js

--- xsijishe.js
+++ xsijishe.js
@@ -1,91 +1,163 @@
 // ==UserScript==
 // @name         xsijishe
 // @namespace    http://tampermonkey.net/
-// @version      20250820
+// @version      20250821
 // @description  try to take over the world!
 // @author       You
-// @match        https://xsijishe.com/home.php?*do=favorite*
-// @match        https://xsijishe.com/forum.php?*mod=forumdisplay*
+// @match        https://xsijishe.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=xsijishe.com
-// @grant        unsafeWindow
 // ==/UserScript==
 
 const get_page = async (url) => {
-    let res = await fetch(url).then((res) => res.text());
-    return new DOMParser().parseFromString(res, "text/html");
+  let res = await fetch(url).then((res) => res.text());
+  return new DOMParser().parseFromString(res, "text/html");
 };
 
+const get_formhash = async (href) => {
+  let doc = await get_page(href);
+  return doc.querySelector('input[name="formhash"]').value;
+};
 
-const delFav = async (href) => {
-    event.preventDefault();
-    let res = await fetch(href).then((res) => res.text());
-    let doc = new DOMParser().parseFromString(res, "text/html");
-    console.log(doc.querySelector('input[name="formhash"]').value);
+const addFav = async (href, referer = location.href) => {
+  event?.preventDefault();
+  let formhash = await get_formhash(href);
 
-    const formData = new FormData();
-    formData.append('referer', "https://xsijishe.com/home.php?mod=space&do=favorite&view=me");
-    formData.append('formhash', doc.querySelector('input[name="formhash"]').value);
-    formData.append('deletesubmit', true);
-    formData.append('deletesubmitbtn', true);
+  const formData = new FormData();
+  formData.append("referer", referer);
+  formData.append("formhash", formhash);
+  formData.append("handlekey", "k_favorite");
+  formData.append("favoritesubmit", true);
 
-    fetch(href, { method: 'POST', body: formData })
-    return false
-}
+  fetch(href, { method: "POST", body: formData });
+  return false;
+};
 
+const delFav = async (href, referer = location.href) => {
+  event?.preventDefault();
+  let formhash = await get_formhash(href);
 
-(function() {
-    unsafeWindow.delFav=delFav;
+  const formData = new FormData();
+  formData.append("referer", referer);
+  formData.append("formhash", formhash);
+  formData.append("deletesubmit", true);
+  formData.append("deletesubmitbtn", true);
 
-    [...document.querySelectorAll('#favorite_ul>li')].forEach(async e => {
-        e.querySelector('a').attributes.onclick.value = "delFav(this.href); this.parentNode?.remove();"
+  fetch(href, { method: "POST", body: formData });
+  return false;
+};
 
-        let href = e.querySelector('a:last-of-type').href
-        let doc = await get_page(href)
-        let srt = "location2={href:''};location2.assign=function(val){this.href=val};location2.replace=function(val){this.href=val};" + doc.scripts[0].innerHTML.replaceAll('location', 'location2') + "location2;"
+const getRealLink = async (href) => {
+  return href;
+  let doc = await get_page(href);
+  let srt =
+    "location2={href:''};location2.assign=function(val){this.href=val};location2.replace=function(val){this.href=val};" +
+    doc.scripts[0].innerHTML.replaceAll("location", "location2") +
+    "location2;";
+  try {
+    let res = window.eval(srt);
+    let newLink = res?.href || res;
+    return newLink;
+  } catch (e) {
+    console.log(href, e, srt);
+    return "";
+  }
+};
 
-        try {
-            let res = window.eval(srt)
-            let newLink = res?.href || res
-            console.log([res, newLink])
+const imgCenter = (cssSeletor) => {
+  document.querySelectorAll(cssSeletor).forEach((div) => {
+    div.style.display = "flex";
+    div.style.alignItems = "center";
+    div.querySelectorAll("img").forEach((img) => {
+      img.style.objectFit = "cover";
+    });
+  });
+};
 
-            // document.getElementById('test')?.remove();
-            // script = document.createElement('script');
-            // script.id = 'test';
-            // script.textContent = srt;
-            // document.body.appendChild(script);
-            // console.log(location2);
+(function () {
+  [
+    ".nex_acgcommon_img",
+    ".nex_newrecos_img",
+    ".nex_ctbpic",
+    ".nexproul>li",
+  ].forEach(imgCenter);
+  document.querySelector(".nex_Product_unextend")?.remove();
+  document.querySelector(".ct2>.sd")?.remove();
+  document.querySelector(".ct2>.mn>.drag")?.remove();
+  document.querySelector(".nex_junctionport")?.remove();
+  document.querySelector("#hornbox")?.remove();
+  document
+    .querySelectorAll(".nex_top_bg")
+    .forEach((e) => (e.style.height = "50px"));
+  document
+    .querySelectorAll(".ct2>.mn")
+    .forEach((e) => e.style.setProperty("width", "1150px", "important"));
+  document.querySelectorAll(".nex_forumlist_pics>ul").forEach((ul) => {
+    ul.parentElement.style.width = "100%";
+    ul.style.display = "flex";
+    ul.style.overflow = "auto";
+    ul.style.width = "100%";
+    ul.querySelectorAll("li").forEach((li) => {
+      li.style.display = "block";
+      li.style.overflow = "unset";
+      li.style.height = "300px";
+      li.style.width = "400px";
+    });
+    ul.querySelectorAll(".nex_thread_pics").forEach((div) => {
+      div.style.height = "300px";
+      div.style.width = "400px";
+      div.style.display = "flex";
+      div.style.alignItems = "center";
+      let src = div.querySelector("a").style.background.split('"')[1];
+      div.innerHTML = `<img id="aimg_2113312" aid="2113312" src="${src}" class="zoom" onclick="zoom(this, this.src, 0, 0, 0)" width="400" style=" object-fit: cover; height: 300px; " initialized="true">`;
+    });
+  });
 
-            let doc2 = await get_page(newLink)
-            let imgText = [...doc2.querySelectorAll('img.zoom')].map(e => { e.src = e.attributes.file.value; return e.outerHTML }).join('')
-            e.insertAdjacentHTML('beforeend', `<div style="overflow:auto"><div style="display: flex; flex-wrap:no-wrap">${imgText}</div></div>`)
-            e.querySelectorAll('img.zoom').forEach(e => { e.style.height = "200px"; e.style.width = "fit-content" })
-        } catch (e) {
-            console.log(href, e, srt)
-        }
-    })
+  document.querySelectorAll("#k_favorite").forEach((a) => {
+    a.onclick = () => addFav(a.href);
+  });
 
-    document.querySelectorAll('.nex_forumlist_pics>ul').forEach(ul=>{
-        ul.style.display = 'flex';
-        ul.style.overflow = 'auto';
-        ul.querySelectorAll('li').forEach(li=>{
-            li.style.display = 'block';
-            li.style.overflow = 'unset';
-            li.style.height = '300px';
-            li.style.width = '400px';
-        });
-        ul.querySelectorAll('.nex_thread_pics').forEach(div=>{
-            div.style.height = '300px';
-            div.style.width = '400px';
-        });
-        ul.querySelectorAll('.nex_thread_pics a').forEach(a=>{
-            a.style.height = '300px';
-            a.style.width = '400px';
-        });
-    })
+  [...document.querySelectorAll("#favorite_ul>li")].forEach(async (li) => {
+    let a = li.querySelector("a");
+    a.onclick = () => {
+      delFav(a.href);
+      li.remove();
+    };
 
-    let html =`<a><span class="nex_reply" style=" background: #F39A07; width: 20px; height: 20px; padding-left: 0px; margin: 0px 10px; "><i style=" background: url(https://sjs47.com/template/nex_manga_181008/neoconex/viewthread/vt_favourite.png) center no-repeat; width: 20px; height: 20px; display: block; "></i></span></a>`
-    document.querySelectorAll('.z.nex_list_infos').forEach(e=>{
-        e.insertAdjacentHTML('beforeend', html)
-    })
+    let href = li.querySelector("a:last-of-type").href;
+    let realLink = await getRealLink(href);
+    if (realLink) {
+      let doc2 = await get_page(realLink);
+      let imgText = [...doc2.querySelectorAll("img.zoom")]
+        .map((e) => {
+          e.src = e.attributes.file?.value;
+          return e.outerHTML;
+        })
+        .join("");
+      li.insertAdjacentHTML(
+        "beforeend",
+        `<div style="overflow:auto"><div style="display: flex; flex-wrap:no-wrap">${imgText}</div></div>`
+      );
+      li.querySelectorAll("img.zoom").forEach((e) => {
+        e.style.height = "300px";
+        e.style.width = "fit-content";
+      });
+    }
+  });
 
+  let html = `<a style=" margin: 0 25px; background: #00B295"><span class="nex_reply">收藏</span></a>  `;
+  document.querySelectorAll(".z.nex_list_infos").forEach((div) => {
+    div.insertAdjacentHTML("beforeend", html);
+    div.querySelector("a").onclick = async (event) => {
+      let href = div.parentElement.parentElement.querySelector("a").href;
+      let realLink = await getRealLink(href);
+      if (realLink) {
+        let doc = await get_page(realLink);
+        let url = doc.querySelector("#k_favorite")?.href;
+        addFav(url, realLink);
+        div.querySelector("a").remove();
+      }
+      event.preventDefault();
+      return false;
+    };
+  });
 })();
