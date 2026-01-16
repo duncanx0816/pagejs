70079a82317ca32986246c227a6ee166657c5680
duncanx0816 <xiongdengkun@163.com>
2026-01-16 13:41:37 +0800

    Update version number in vixen.js
    fix the api path

--- vixen.js
+++ vixen.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         vixen
 // @namespace    http://tampermonkey.net/
-// @version      2025-09-06
+// @version      2026-01-06
 // @description  try to take over the world!
 // @author       You
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=vixen.com
